fb5ae9843afb6c5442ba79434335a5ef6afa7075
Lukas201607 <132807700+Lukas201607@users.noreply.github.com>
2023-05-07 09:13:27 +0200

    Aktualizovať main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,3 @@
-input.onGesture(Gesture.Shake, function () {
-    basic.showIcon(IconNames.Heart)
-})
 basic.forever(function () {
     basic.showString("Hello!")
     basic.showLeds(`
@@ -14,11 +11,9 @@ basic.forever(function () {
     RingbitCar.running_distance(RingbitCar.Direction_run.forward, 10)
     RingbitCar.forward()
     RingbitCar.brake()
+    basic.showIcon(IconNames.Heart)
     RingbitCar.turnleft()
     RingbitCar.steering_angle(RingbitCar.Direction_turn.right, 20)
     RingbitCar.back()
     RingbitCar.freestyle(15, 15)
-})
-basic.forever(function () {
-	
 })
